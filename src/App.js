8670583ef826713d9de8198ee47268bfc82b7e1c
Roz Gillie <rgillie@uw.edu>
2022-02-18 16:45:06 -0800

    added search bar and titles to home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchBar from './SearchBar';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="header">
+      <div className="logo"/>
+      <span className="title">Gender Gap Analysis</span> 
+    </header>
+    <body>
+      <h1>Creating Gender Gap Transparency</h1>
+    <SearchBar/>
+    </body>
+    <footer>
+    </footer>
+  </div>
   );
 }
 
---
+++ src/SearchBar.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Autocomplete from '@mui/material/Autocomplete';
+
+// referred to MUI website for search bar code with autocomplete (https://mui.com/components/autocomplete/)
+
+function FreeSolo() {
+    return (
+      <Stack spacing={2} sx={{ width: 300 }}>
+        <Autocomplete
+          id="free-solo-demo"
+          freeSolo
+          options={companies.map((option) => option.name)}
+          renderInput={(params) => <TextField {...params} label="Search for a Company..." />}
+        />
+      </Stack>
+    );
+  }
+
+  // list of company options for search bar
+  const companies = [
+      {name: "A"},
+      {name: "B"}
+  ]
+ 
+export default FreeSolo;
